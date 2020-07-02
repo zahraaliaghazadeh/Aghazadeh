36beda082a2855bacf3cda8baf22b0837d4b7914
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-07-02 16:15:19 -0700

    set up material UI

--- client/src/pages/SplashPage/index.js
+++ client/src/pages/SplashPage/index.js
@@ -2,13 +2,27 @@ import React from 'react'
 // about page style
 import "./style.css"
 import API from "../../utils/API"
+import Button from '@material-ui/core/Button';
 
 
 export default function SplashPage() {
     return (
-        <h1>
+        <div>
 "Helloo"
-        </h1>
+<Button variant="contained">Default</Button>
+<Button variant="contained" color="primary">
+  Primary
+</Button>
+<Button variant="contained" color="secondary">
+  Secondary
+</Button>
+<Button variant="contained" disabled>
+  Disabled
+</Button>
+<Button variant="contained" color="primary" href="#contained-buttons">
+  Link
+</Button>
+        </div>
 
         )
     }

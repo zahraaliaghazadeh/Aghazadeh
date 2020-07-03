0e4c047517b1d7618f497ed1d2f8f3b6e53a29be
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-07-03 13:12:04 -0700

    made the initial form request

--- client/src/pages/RequestServicePage/index.js
+++ client/src/pages/RequestServicePage/index.js
@@ -2,13 +2,34 @@ import React from 'react'
 // about page style
 import "./style.css"
 import API from "../../utils/API"
+import TextField from '@material-ui/core/TextField';
+
 
 
 export default function RequestServicePage() {
     return (
-        <h1>
-"Helloo"
-        </h1>
+<div>
+<form  noValidate autoComplete="off">
+      <div>
+        <TextField required id="standard-required" label="Required" defaultValue="Name نام" /> <br></br>
+        <TextField required id="standard-required" label="Required" defaultValue="Phone Number شماره تلفن" /> <br></br>
+        <TextField required id="standard-required" label="Required" defaultValue="Device Name نام دستگاه" /> <br></br>
+        <TextField required id="standard-required" label="Required" defaultValue="Brand برند" /> <br></br>
+        <TextField required id="standard-textarea" label="Required" defaultValue="Address آدرس" /> <br></br>
+        <TextField required id="standard-required" label="Required" defaultValue="Problem مشکل" /> <br></br>
+        <TextField
+          id="standard-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+       
+        
+      
+      </div>
+    </form>
+</div>
 
         )
     }

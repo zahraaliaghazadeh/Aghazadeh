7e4f0f15ce03264d0b5ec9bb45b9502dbbfd0b0d
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-07-03 13:29:47 -0700

    added the 2 cards to the splash page and imported card on top

--- client/src/pages/SplashPage/index.js
+++ client/src/pages/SplashPage/index.js
@@ -2,27 +2,71 @@ import React from 'react'
 // about page style
 import "./style.css"
 import API from "../../utils/API"
-import Button from '@material-ui/core/Button';
+
+
+// --start of import for card
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+
+// --end of import for card
+
+// -----for card style
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+// -----end of card style
 
 
 export default function SplashPage() {
-    return (
-        <div>
-"Helloo"
-<Button variant="contained">Default</Button>
-<Button variant="contained" color="primary">
-  Primary
-</Button>
-<Button variant="contained" color="secondary">
-  Secondary
-</Button>
-<Button variant="contained" disabled>
-  Disabled
-</Button>
-<Button variant="contained" color="primary" href="#contained-buttons">
-  Link
-</Button>
-        </div>
 
-        )
-    }
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <div>
+      {/* sales */}
+      <Card className={classes.root}>
+        <CardContent>
+
+        <Typography variant="h5" component="h2">
+           Sales فروشگاه
+        </Typography>
+
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+
+
+      {/* service */}
+      <Card className={classes.root}>
+        <CardContent>
+        
+          <Typography variant="h5" component="h2">
+           Service تعمیرگاه
+        </Typography>
+        
+
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>
+
+  )
+}

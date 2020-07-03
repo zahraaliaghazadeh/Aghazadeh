0728460712b17a35e71f0be4a44ee743b2f86018
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-07-03 13:41:38 -0700

    added a card to products page

--- client/src/pages/ProductsPage/index.js
+++ client/src/pages/ProductsPage/index.js
@@ -3,12 +3,56 @@ import React from 'react'
 import "./style.css"
 import API from "../../utils/API"
 
+import { Card, CardActions, CardContent, Button, Typography, CardActionArea, CardMedia } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
 
 export default function ProductsPage() {
+    const classes = useStyles();
     return (
-        <h1>
-"Helloo"
-        </h1>
+ <div>
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://place-hold.it/300"
+          title="place holder image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Cupper Tube
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+           It is a good tube
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+         brand
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          $100 ۱۰۰۰۰ تومان
+          </Typography>
+          
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+       
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+ </div>
 
         )
     }
+
+    

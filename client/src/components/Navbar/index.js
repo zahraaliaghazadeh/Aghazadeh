e9730e9f158d81f72ebb214a76b9508ad93b8661
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-07-06 14:14:18 -0700

    wip

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -19,8 +19,6 @@ export default function Navbar(props) {
                     <li><a href="/contact">Contact</a></li>
                     <li><a href="/services">Services</a></li>
                     <li><a href="/signup">Sign Up</a></li>
-                    <li><a href="/products">Products</a></li>
-                    <li><a href="/contact">Contact</a></li>
                     <li><a href="">About</a></li>
                     
                 </ul>

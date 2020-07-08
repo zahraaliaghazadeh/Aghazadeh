62eea577cd3ff6a872aa0dc5befd4840c6cd3b76
sali6798 <shaidee@alingcastre.net>
2020-07-07 23:14:18 -0700

    product and user controllers, queries for products

---
+++ controllers/productsController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function (req, res) {
+        db.Product
+            .find()
+            .sort({ name: 1 })
+            .then(result => res.json(result))
+            .catch(err => res.status(422).json(err))
+    },
+    findById: function (req, res) {
+        db.Product
+            .findById(req.params.id)
+            .then(product => res.json(product))
+            .catch(err => res.status(422).json(err))
+    },
+    create: function (req, res) {
+        db.Product
+            .create(req.body)
+            .then(product => res.json(product))
+            .catch(err => res.status(422).json(err))
+    },
+    update: function (req, res) {
+        db.Product
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(product => res.json(product))
+            .catch(err => res.status(422).json(err))
+
+    },
+    remove: function (req, res) {
+        db.Product
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};

f5d05fe3abca9d2a90313c490b0e8c2268c7f368
sali6798 <shaidee@alingcastre.net>
2020-07-07 23:17:38 -0700

    imported form and added button

--- client/src/pages/AddProductPage/index.js
+++ client/src/pages/AddProductPage/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Button } from '@material-ui/core';
+import ProductInfoForm from "../../components/ProductInfoForm"
 
 export default function AddProductPage() {
     return (
         <div>
-            
+            <ProductInfoForm />
+            <Button variant="contained">Add Product</Button>
         </div>
     );
 }

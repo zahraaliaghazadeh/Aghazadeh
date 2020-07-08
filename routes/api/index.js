8afd1094b1a535098a3cd681d1500cbe9fb4a457
sali6798 <shaidee@alingcastre.net>
2020-07-07 23:20:27 -0700

    imported product routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-// const bookRoutes = require("./books");
+const productRoutes = require("./product");
 
-// Book routes
-// router.use("/books", bookRoutes);
+// Product routes
+router.use("/product", productRoutes);
 
 module.exports = router;

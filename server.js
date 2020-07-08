86a485113d36f09b2e5217aa9373f6dce2e59ddd
sali6798 <shaidee@alingcastre.net>
2020-07-07 23:15:32 -0700

    mongo constructor config

--- server.js
+++ server.js
@@ -16,9 +16,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/aghazadeh");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/aghazadeh",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`API Server now listening on PORT ${PORT}!`);
 });

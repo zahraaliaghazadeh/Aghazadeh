f528917992a1e4cdc21f4cc6e915959b117443fd
sali6798 <shaidee@alingcastre.net>
2020-07-07 23:16:19 -0700

    added route for admin product creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,5 @@
-// import React from 'react';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello World</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// ==============================
-
 // to use hooks /functional components instead of classes
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 // for routes in React
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
@@ -24,6 +10,7 @@ import ServiceDeptPage from "./pages/ServiceDeptPage"
 import SignUpPage from "./pages/SignUpPage";
 import SplashPage from './pages/SplashPage';
 import ContactPage from './pages/ContactPage';
+import AddProductPage from "./pages/AddProductPage"
 
 // importing components
 import Navbar from "./components/Navbar"
@@ -33,25 +20,12 @@ import API from "./utils/API"
 // the main css file
 import './App.css';
 
-
-
-
-
-
-
 function App() {
- 
-  
-  
   return (
     // all the routes
     <Router>
       <Navbar />
-      
       <Switch>
-        <Route exact path="/">
-          <SplashPage />
-        </Route>
         <Route exact path="/products">
           <ProductsPage />
         </Route>
@@ -69,7 +43,12 @@ function App() {
         <Route exact path="/contact">
           <ContactPage/>
         </Route>
-        
+        <Route exact path="/admin/new">
+          <AddProductPage />
+        </Route>
+        <Route exact path="*">
+          <SplashPage />
+        </Route>
       </Switch>
       <Footer/>
     </Router>

859686ddb04e42878c4425b1e9097fe8ae1decaf
sali6798 <shaidee@alingcastre.net>
2020-07-14 15:45:37 -0700

    removed div

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -11,30 +11,28 @@ import { Button, Typography } from '@material-ui/core';
 export default function Navbar(props) {
 
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li><img id="logo" src={logo1} alt="logo" height="50px" width="80px"></img></li>
-                    <li><a href="/">Home<br></br>
+        <nav>
+            <ul>
+                <li><img id="logo" src={logo1} alt="logo" height="50px" width="80px"></img></li>
+                <li><a href="/">Home<br></br>
  صفحه اصلی</a></li>
-                    <li><a href="/products">Products<br></br>
+                <li><a href="/products">Products<br></br>
  محصولات</a></li>
-                    <li><a href="/contact">Contact<br></br>
+                <li><a href="/contact">Contact<br></br>
  تماس</a></li>
-                    <li><a href="/services">Services<br></br>
+                <li><a href="/services">Services<br></br>
  خدمات</a></li>
-                    <li><a href="/request">Request<br></br>
+                <li><a href="/request">Request<br></br>
  درخواست</a></li>
-                    <li><a href="/signup">Sign Up<br></br>
+                <li><a href="/signup">Sign Up<br></br>
  حساب کاربری</a></li>
-                    <li><a href="/about">About<br></br>
+                <li><a href="/about">About<br></br>
  درباره ما</a></li>
-                    <Button variant="contained" color="primary">
-                        SignUp/Login<br></br>
+                <Button variant="contained" color="primary">
+                    SignUp/Login<br></br>
                         ورود به حساب کاربری </Button>
 
-                </ul>
-            </nav>
-        </div>
+            </ul>
+        </nav>
     );
 }

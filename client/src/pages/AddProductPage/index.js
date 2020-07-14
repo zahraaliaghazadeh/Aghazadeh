a0e4370e4567de76cd6d34e00f287b497116aaf1
sali6798 <shaidee@alingcastre.net>
2020-07-14 15:47:50 -0700

    added class name and wip button

--- client/src/pages/AddProductPage/index.js
+++ client/src/pages/AddProductPage/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from '@material-ui/core';
 import ProductInfoForm from "../../components/ProductInfoForm"
+import "./style.css"
 
 export default function AddProductPage() {
     return (
-        <div>
+        <div className="AddProductPage">
             <ProductInfoForm />
             <Button variant="contained">Add Product</Button>
         </div>

eb50206f13e32c0a3994baf31d29780f2d8cf111
sali6798 <shaidee@alingcastre.net>
2020-07-15 15:38:35 -0700

    add commented out cloudinary code

--- client/src/pages/AddProductPage/index.js
+++ client/src/pages/AddProductPage/index.js
@@ -7,8 +7,28 @@ import "./style.css"
 export default function AddProductPage() {
     const [productInfo, setProductInfo] = useState({});
 
-    const handleInputChange = event => {
+    const handleInputChange = async event => {
         const { name, value } = event.target;
+
+        // const files = e.target.files;
+        // const data = new FormData();
+        // data.append("file", files[0]);
+        // // this data/preset is required by cloudinary (named sick fits in the cloudinary settings)
+        // data.append("upload_preset", "k0kdipbb");
+        // const res = await fetch(
+        //     "https://api.cloudinary.com/v1_1/dw69fw1u3/image/upload",
+        //     {
+        //         method: "POST",
+        //         body: data
+        //     }
+        // );
+        // const file = await res.json();
+        // console.log(file);
+        // setProductState({
+        //     ...productState, image: file.secure_url
+        //     // largeImage: file.eager[0].secure_url
+        // });
+
         setProductInfo({...productInfo, [name]: value});
     }
 

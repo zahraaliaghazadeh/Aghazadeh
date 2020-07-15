a5625ee2b3c13c0c41f111d8101c9f297e4e16a8
sali6798 <shaidee@alingcastre.net>
2020-07-15 13:39:04 -0700

    handle input change and form submission

--- client/src/pages/AddProductPage/index.js
+++ client/src/pages/AddProductPage/index.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from '@material-ui/core';
 import ProductInfoForm from "../../components/ProductInfoForm"
+import API from "../../utils/API"
 import "./style.css"
 
 export default function AddProductPage() {
+    const [productInfo, setProductInfo] = useState({});
+
+    const handleInputChange = event => {
+        const { name, value } = event.target;
+        setProductInfo({...productInfo, [name]: value});
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        API.createProduct(productInfo)
+            .then(({ data }) => console.log(data))
+            .catch(err => console.log(err))
+        
+        setProductInfo({})
+    }
+
     return (
-        <div className="AddProductPage">
-            <ProductInfoForm />
-            <Button variant="contained">Add Product</Button>
-        </div>
+        <form className="AddProductPage" onSubmit={handleSubmit}>
+            <ProductInfoForm {...productInfo} handleInputChange={handleInputChange} />
+            <Button variant="contained" type="submit">Add Product</Button>
+        </form>
     );
 }

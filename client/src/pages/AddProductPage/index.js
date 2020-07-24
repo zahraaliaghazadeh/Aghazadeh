c8dc16ae24f8e7a02d329cafce19f7a472b9e90b
sali6798 <shaidee@alingcastre.net>
2020-07-24 14:41:56 -0700

    POST for product info with cloudinary working

--- client/src/pages/AddProductPage/index.js
+++ client/src/pages/AddProductPage/index.js
@@ -13,57 +13,42 @@ export default function AddProductPage() {
             setProductInfo({ ...productInfo, picture: null });
         }
         else {
-            console.log("hi")
             setProductInfo({ ...productInfo, [name]: name === "picture" ? event.target.files[0] : value });
         }
-
-        // if (name === "picture") {
-        //     const file = e.target.files[0];
-
-        //     if (file) {
-        //         const reader = new FileReader();
-
-        //         reader.readAsDataURL(file)
-        //         reader.onload = () => setPicture(reader.result);
-        //         // setPicture(URL.createObjectURL(e.target.files[0]))
-        //     }
-        // }
-        // else {
-        //     setProductInfo({ ...productInfo, [name]: name === "picture" ? event.target.files[0] : value });
-        // }
-
     }
 
     const handleSubmit = async event => {
         event.preventDefault();
 
-        if (productInfo.picture) {
-            // const files = e.target.files;
-            const data = new FormData();
-            data.append("file", productInfo.picture);
-            // this data/preset is required by cloudinary (named sick fits in the cloudinary settings)
-            data.append("upload_preset", "k0kdipbb");
-            const res = await fetch(
-                "https://api.cloudinary.com/v1_1/dw69fw1u3/image/upload",
-                {
-                    method: "POST",
-                    body: data
-                }
-            );
-            const file = await res.json();
-            console.log(file);
+        let image;
+
+        try {
+            if (productInfo.picture) {
+                const formData = new FormData();
+                formData.append("file", productInfo.picture);
+                // this data/preset is required by cloudinary (named sick fits in the cloudinary settings)
+                formData.append("upload_preset", "k0kdipbb");
+                const res = await fetch(
+                    "https://api.cloudinary.com/v1_1/dw69fw1u3/image/upload",
+                    {
+                        method: "POST",
+                        body: formData
+                    }
+                );
+                
+                const file = await res.json();
+                console.log(file);
+                image = file.secure_url;
+            }
+
+            const { data } = await API.createProduct({...productInfo, image: image });
+            console.log(data);
+
+            setProductInfo({});
+
+        } catch (error) {
+            console.log(error)
         }
-
-        // setProductState({
-        //     ...productState, image: file.secure_url
-        //     // largeImage: file.eager[0].secure_url
-        // });
-
-        // API.createProduct(productInfo)
-        //     .then(({ data }) => console.log(data))
-        //     .catch(err => console.log(err))
-
-        setProductInfo({})
     }
 
     return (

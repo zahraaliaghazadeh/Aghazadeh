260a30d6c1d8202eed19515967b2102860518734
sali6798 <shaidee@alingcastre.net>
2020-07-24 17:12:37 -0700

    added form, and remove/update methods

--- client/src/pages/ProductInfoPage/index.js
+++ client/src/pages/ProductInfoPage/index.js
@@ -1,9 +1,65 @@
 import React from "react";
+import API from "../../utils/API";
 
-export default function ProductInfoPage() {
-    return (
-        <div>
+export default function ProductInfoPage(props) {
+    const [productInfo, setProductInfo] = useState({});
+
+    const handleInputChange = async (event, remove) => {
+        const { name, value } = event.target;
+
+        if (remove) {
+            setProductInfo({ ...productInfo, picture: null });
+        }
+        else {
+            setProductInfo({ ...productInfo, [name]: name === "picture" ? event.target.files[0] : value });
+        }
+    }
+
+    const handleRemove = () => {
+        API.deleteProduct() // TODO: pass id 
+            .then(({ data }) => console.log(data))
+            .catch(err => console.log(err));
+
+        // redirect
+    }
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+
+        let image;
+
+        try {
+            if (productInfo.picture && typeof productInfo.picture === "object") {
+                const formData = new FormData();
+                formData.append("file", productInfo.picture);
+                // this data/preset is required by cloudinary (named sick fits in the cloudinary settings)
+                formData.append("upload_preset", "k0kdipbb");
+                const res = await fetch(
+                    "https://api.cloudinary.com/v1_1/dw69fw1u3/image/upload",
+                    {
+                        method: "POST",
+                        body: formData
+                    }
+                );
+
+                const file = await res.json();
+                console.log(file);
+                image = file.secure_url;
+            }
             
+            const { data } = await API.updateProduct({ ...productInfo, image: image }); // TODO: pass id
+            console.log(data);
+
+            setProductInfo({});
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div className="AddProductPage">
+            <ProductInfoForm {...productInfo} handleInputChange={handleInputChange} handleSubmit={handleSubmit} handleRemove={handleRemove} />
         </div>
     );
 }
